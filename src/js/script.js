2fbe8d7342eb70239e9654962e6102e29b8f8097
Klochette <melina.chamayou@gmail.com>
2020-01-09 20:42:21 +0100

    fix bugs game

--- src/js/script.js
+++ src/js/script.js
@@ -34,13 +34,6 @@ function home() {
 }
 
 function game() {
-  let audio = document.querySelector(".audio");
-  soundIcon = document.querySelector(".soundControlIcon");
-  audio.play();
-  soundIcon.addEventListener("click", function() {
-    mute();
-  });
-
   let character;
   let speedObj = 4;
   let speedJump = 1.2;
@@ -55,159 +48,175 @@ function game() {
       e.preventDefault();
     }
   });
-    character = document.getElementById("character");
-    //  Give the ability of moving to the character
-    function jump(character) {
-      oxo.inputs.listenKey("up", function() {
-        if (!character.classList.contains("jump")) {
-          character.classList.add("jump");
-          setTimeout(() => {
-            character.classList.remove("jump");
-          }, intervalJump);
-        }
-      });
-      oxo.inputs.listenKey("down", function() {
-        if (!character.classList.contains("bend")) {
-          character.classList.add("bend");
-          setTimeout(() => {
-            character.classList.remove("bend");
-          }, intervalBend);
-        }
-      });
-    }
-
-    //    Create an obstacle like a chair, a table or a coffee dropped on the floor
-    function createObstacle() {
-      let i;
-      let obs;
-      i = oxo.utils.getRandomNumber(1, 4);
-      if (i === 1) {
-        obs = oxo.elements.createElement({
-          class: "obstacles obstacles__table obstacles__move",
-          obstacle: true,
-          appendTo: ".try"
-        });
-        obs.setAttribute("id", "obstacle");
-      } else if (i === 2) {
-        obs = oxo.elements.createElement({
-          class: "obstacles obstacles__chair obstacles__move",
-          obstacle: true,
-          appendTo: ".try"
-        });
-        obs.setAttribute("id", "obstacle");
-      } else if (i === 3) {
-        obs = oxo.elements.createElement({
-          class: "obstacles obstacles__coffee obstacles__move",
-          obstacle: true,
-          appendTo: ".try"
-        });
-        obs.setAttribute("id", "obstacle");
-      } else if (i === 4) {
-        obs = oxo.elements.createElement({
-          class: "obstacles obstacles__proj obstacles__move",
-          obstacle: true,
-          appendTo: ".try"
-        });
-        obs.setAttribute("id", "obstacle");
+  character = document.getElementById("character");
+  //  Give the ability of moving to the character
+  function jump(character) {
+    oxo.inputs.listenKey("up", function() {
+      if (!character.classList.contains("jump")) {
+        character.classList.add("jump");
+        setTimeout(() => {
+          character.classList.remove("jump");
+        }, intervalJump);
       }
-      oxo.elements.onCollisionWithElement(character, obs, function() {
-        // when the obstacle hits James, GAME OVER
-        if (invicible === false) {
-          console.log("gameover");
-          obs.remove();
-        }
+    });
+    oxo.inputs.listenKey("down", function() {
+      if (!character.classList.contains("bend")) {
+        character.classList.add("bend");
+        setTimeout(() => {
+          character.classList.remove("bend");
+        }, intervalBend);
+      }
+    });
+  }
+
+  //    Create an obstacle like a chair, a table or a coffee dropped on the floor
+  function createObstacle() {
+    let i;
+    let obs;
+    i = oxo.utils.getRandomNumber(1, 4);
+    if (i === 1) {
+      obs = oxo.elements.createElement({
+        class: "obstacles obstacles__table obstacles__move",
+        obstacle: true,
+        appendTo: ".try"
       });
-      oxo.elements.onLeaveScreenOnce(
-        //when the obstacle leaves the screen, the div which contains the obstacle is removed
-        obs,
-        function() {
-          obs.remove();
-        },
-        true
-      );
-      obs.style.animationDuration = speedObj + "s";
-      character.style.animationDuration = speedJump + "s";
+      obs.setAttribute("id", "obstacle");
+    } else if (i === 2) {
+      obs = oxo.elements.createElement({
+        class: "obstacles obstacles__chair obstacles__move",
+        obstacle: true,
+        appendTo: ".try"
+      });
+      obs.setAttribute("id", "obstacle");
+    } else if (i === 3) {
+      obs = oxo.elements.createElement({
+        class: "obstacles obstacles__coffee obstacles__move",
+        obstacle: true,
+        appendTo: ".try"
+      });
+      obs.setAttribute("id", "obstacle");
+    } else if (i === 4) {
+      obs = oxo.elements.createElement({
+        class: "obstacles obstacles__proj obstacles__move",
+        obstacle: true,
+        appendTo: ".try"
+      });
+      obs.setAttribute("id", "obstacle");
     }
+    oxo.elements.onCollisionWithElement(character, obs, function() {
+      // when the obstacle hits James, GAME OVER
+      if (invicible === false) {
+        oxo.screens.loadScreen("end", function() {
+          clearIntervals();
+          end();
+        });
+        obs.remove();
+      }
+    });
+    oxo.elements.onLeaveScreenOnce(
+      //when the obstacle leaves the screen, the div which contains the obstacle is removed
+      obs,
+      function() {
+        obs.remove();
+      },
+      true
+    );
+    obs.style.animationDuration = speedObj + "s";
+    character.style.animationDuration = speedJump + "s";
+  }
 
-    // creates an enemy
-    function createEnnemy() {
-      let e = oxo.elements.createElement({
-        class: "ennemy ennemy__move",
-        appendTo: ".try",
-        obstacle: true
-      });
+  // creates an enemy
+  function createEnnemy() {
+    let e = oxo.elements.createElement({
+      class: "ennemy ennemy__move",
+      appendTo: ".try",
+      obstacle: true
+    });
 
-      // increase difficulty
-      oxo.elements.onCollisionWithElement(character, e, function() {
-        if (speedObj > 1) speedObj -= 0.25; // console.log("object takes " + speedObj + "s to get out");
-        if (intervalObj >= 500) intervalObj -= 500; // console.log("object takes " + intervalObj + "ms to come");
-        if (speedJump >= 0.45) speedJump = speedJump - 0.05; // console.log("jump is set to " + speedJump);
-        if (intervalBend >= 450) intervalBend = intervalBend - 50; // console.log("bend is set to " + intervalBend);
-        characterPosition = 50;
+    // increase difficulty
+    oxo.elements.onCollisionWithElement(character, e, function() {
+      if (speedObj > 1) speedObj -= 0.25; // console.log("object takes " + speedObj + "s to get out");
+      if (intervalObj >= 500) intervalObj -= 500; // console.log("object takes " + intervalObj + "ms to come");
+      if (speedJump >= 0.45) speedJump = speedJump - 0.05; // console.log("jump is set to " + speedJump);
+      if (intervalBend >= 450) intervalBend = intervalBend - 50; // console.log("bend is set to " + intervalBend);
+      characterPosition = 50;
+      character.style.left = characterPosition + "px";
+      oxo.player.addToScore(7500);
+      e.remove();
+    });
+  }
+
+  function createBonusWater() {
+    let bonus;
+    bonus = oxo.elements.createElement({
+      class: "bonus bonus__move bonus--water",
+      appendTo: ".try",
+      obstacle: true
+    });
+    oxo.elements.onLeaveScreenOnce(
+      bonus,
+      function() {
+        bonus.remove();
+      },
+      true
+    );
+    oxo.elements.onCollisionWithElement(character, bonus, function() {
+      bonus.remove();
+      if (characterPosition <= 350) {
+        characterPosition += 100;
         character.style.left = characterPosition + "px";
-        oxo.player.addToScore(7500);
-        e.remove();
-      });
-    }
-
-    function createBonusWater() {
-      let bonus;
-      bonus = oxo.elements.createElement({
-        class: "bonus bonus__move bonus--water",
-        appendTo: ".try",
-        obstacle: true
-      });
-      oxo.elements.onLeaveScreenOnce(
-        bonus,
-        function() {
-          bonus.remove();
-        },
-        true
-      );
-      oxo.elements.onCollisionWithElement(character, bonus, function() {
+      }
+    });
+  }
+  function createBonusBirdies() {
+    let bonus;
+    bonus = oxo.elements.createElement({
+      class: "bonus bonus__move bonus--birdies",
+      appendTo: ".try",
+      obstacle: true
+    });
+    oxo.elements.onLeaveScreenOnce(
+      bonus,
+      function() {
         bonus.remove();
-        if (characterPosition <= 350) {
-          characterPosition += 100;
-          character.style.left = characterPosition + "px";
-        }
-      });
-    }
-    function createBonusBirdies() {
-      let bonus;
-      bonus = oxo.elements.createElement({
-        class: "bonus bonus__move bonus--birdies",
-        appendTo: ".try",
-        obstacle: true
-      });
-      oxo.elements.onLeaveScreenOnce(
-        bonus,
-        function() {
-          bonus.remove();
-        },
-        true
-      );
-      oxo.elements.onCollisionWithElement(character, bonus, function() {
-        bonus.remove();
-        invicible = true;
-        character.classList.add("invicible");
-        setTimeout(function() {
-          invicible = false;
-          character.classList.remove("invicible");
-        }, 5000);
-      });
-    }
+      },
+      true
+    );
+    oxo.elements.onCollisionWithElement(character, bonus, function() {
+      bonus.remove();
+      invicible = true;
+      character.classList.add("invicible");
+      setTimeout(function() {
+        invicible = false;
+        character.classList.remove("invicible");
+      }, 5000);
+    });
+  }
 
-    function score() {
-      oxo.player.setScore(0);
-    }
+  function score() {
+    oxo.player.setScore(0);
+  }
 
-    createEnnemy();
-    jump(character);
-    setInterval(createObstacle, intervalObj);
-    setInterval(createEnnemy, 30000);
-    setInterval(createBonusWater, 7000);
-    setInterval(createBonusBirdies, 20000);
-    score();
+  function clearIntervals() {
+    clearInterval(intervalObstacle);
+    clearInterval(intervalEnnemy);
+    clearInterval(intervalBonusWater);
+    clearInterval(intervalBonusBirdies);
+  }
+
+  createEnnemy();
+  jump(character);
+  let intervalObstacle = setInterval(createObstacle, intervalObj);
+  let intervalEnnemy = setInterval(createEnnemy, 30000);
+  let intervalBonusWater = setInterval(createBonusWater, 7000);
+  let intervalBonusBirdies = setInterval(createBonusBirdies, 20000);
+  score();
+  /*let audio = document.querySelector(".audio");
+  soundIcon = document.querySelector(".soundControlIcon");
+  audio.play();
+  soundIcon.addEventListener("click", function() {
+    mute();
+  });*/
 }
 
 function end() {

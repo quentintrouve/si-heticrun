3b0930ed21dcd24420457fd516f7ec80c2bd0698
quentintrouve <trouve.quentin@gmail.com>
2020-01-09 08:58:08 +0100

    create game stylesheet add img load screen = ok

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
-oxo.screens.loadScreen("home", function() {});
+oxo.screens.loadScreen("howToPlay", function() {});
+
+/*function setClick(element, destination, callback) {
+  document.querySelector(element).addEventListener("click", function() {
+    oxo.screens.loadScreen(destination, callback);
+  });
+}
+
+oxo.screens.loadScreen("home", home);
+
+function home() {
+  setClick(".homePage__buttonPlay", "game", game);
+  setClick(".homePage__buttonHowToPlay", "howToPlay", howToPlay);
+  setClick(".homePage__buttonCredits", "credits", credits);
+}
+
+function game() {}
+
+function end() {
+  setClick(".endGame__buttonRestart", "game", game);
+  setClick(".homePageIcon", "home", home);
+}
+
+function credits() {
+  setClick(".homePageIcon", "home", home);
+}*/
